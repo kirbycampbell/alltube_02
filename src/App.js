fed7fe5188c479f6170d78b319624568e61470fb
kirbycampbell <jkirbycampbell@gmail.com>
2018-12-18 10:59:17 -0800

    Setup Amplify

--- src/App.js
+++ src/App.js
@@ -1,28 +1,72 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Amplify, { Storage, API, graphqlOperation } from "aws-amplify";
+import aws_exports from "./aws-exports";
+import { withAuthenticator, S3Album } from "aws-amplify-react";
+Amplify.configure(aws_exports);
+Storage.configure({ level: "private" });
+
+const listTodos = `query listTodos {
+  listTodos{
+    items{
+      id
+      name
+      description
+    }
+  }
+}`;
+
+const addTodo = `mutation createTodo($name:String! $description: String!) {
+  createTodo(input:{
+    name:$name
+    description:$description
+  }){
+    id
+    name
+    description
+  }
+}`;
 
 class App extends Component {
+  uploadFile = evt => {
+    const file = evt.target.files[0];
+    const name = file.name;
+
+    Storage.put(name, file).then(() => {
+      this.setState({ file: name });
+    });
+  };
+
+  componentDidMount() {
+    console.log("Component Mounted");
+  }
+
+  todoMutation = async () => {
+    const todoDetails = {
+      name: "Party tonight!",
+      description: "Amplify CLI rocks!"
+    };
+
+    const newEvent = await API.graphql(graphqlOperation(addTodo, todoDetails));
+    alert(JSON.stringify(newEvent));
+  };
+
+  listQuery = async () => {
+    console.log("listing todos");
+    const allTodos = await API.graphql(graphqlOperation(listTodos));
+    alert(JSON.stringify(allTodos));
+  };
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <p> Pick a file</p>
+        <input type="file" onChange={this.uploadFile} />
+        <button onClick={this.listQuery}>GraphQL Query</button>
+        <button onClick={this.todoMutation}>GraphQL Mutation</button>
+        <S3Album level="private" path="" />
       </div>
     );
   }
 }
 
-export default App;
+export default withAuthenticator(App, true);
